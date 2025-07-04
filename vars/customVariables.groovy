800b56899a00be67c3c2e081507a051f97a3cad9
munawarraza <munawarraza068@gmail.com>
2025-07-05 00:32:08 +0500

    add new file

--- vars/customVariables.groovy
+++ vars/customVariables.groovy
@@ -3,9 +3,19 @@
 //    return myvars
 //}
 
-def ENVIRONMENT = {
-    'dev': 'Development Environment',
-    'qa': 'Quality Assurance Environment',
-    'prod': 'Production Environment'
+//def ENVIRONMENT = {
+//    'dev': 'Development Environment',
+//    'qa': 'Quality Assurance Environment',
+//    'prod': 'Production Environment'
 // def Email="munawarraza068@gmail.com"
+//}
+
+def call() {
+  return [
+    ENVIRONMENTS: ['qa', 'staging', 'prod'],
+    DOCKER_REGISTRY: 'docker.oneloadpk.com',
+    IMAGE_VERSION: 'v1.0.0',
+    DEPLOY_TIMEOUT: 300,
+    // add more variables as needed
+  ]
 }
