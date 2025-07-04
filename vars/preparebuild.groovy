9f0a6af4855efad843dbc777c55190f1594f4bde
munawarraza <munawarraza068@gmail.com>
2025-07-05 02:23:56 +0500

    add

--- vars/preparebuild.groovy
+++ vars/preparebuild.groovy
@@ -1,10 +1,11 @@
-def call(){
+def call(Map config){
     script {
 sh"""
     echo tag_name: '${params.TAG}' >>tags.txt
     echo job_name: '${JOB_NAME}' >>tags.txt
     echo env_name: '${params.ENVIRONMENTS}' >>tags.txt
     echo workspace_name: '${WORKSPACE}' >>tags.txt
+    echo Docker Registory: '${config.DOCKER_REGISTRY}' >>tags.txt
 """
     }
 }
