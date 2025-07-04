76ddf34e2d42a6d78264be22fa4b2a4cdfca4bab
munawarraza <munawarraza068@gmail.com>
2025-07-05 03:15:53 +0500

    add

--- vars/preparebuild.groovy
+++ vars/preparebuild.groovy
@@ -12,6 +12,28 @@ sh"""
     echo workspace_name: '${WORKSPACE}' >>tags.txt
     echo Docker Registory: '${config.DOCKER_REGISTRY}' >>tags.txt
     cp -r '${config.SCRIPTS_DIR}'/'${config.DEPLOYMENT_FILE}' .
+
+    echo ### Setup Inventory file for the deployment ###
+    cp -r '${INVENTOR_DIR}'/'${ANSIBLE_INVENTORY_FILE}' .
+   
+    sed -i 's/target/${params.ENVIRONMENTS}/g' '${ANSIBLE_INVENTORY_FILE}'
+    
+    sed -i 's/ip/${config.SERVICE_IPS[params.ENVIRONMENTS]}/g' '${ANSIBLE_INVENTORY_FILE}'
+   
+  #  if [ '${params.ENVIRONMENTS}' = 'dev' ]; then
+  #     sed -i 's/ip/${DEV_IP}/g' '${ANSIBLE_INVENTORY_FILE}'
+  #  elif [ '${params.ENVIRONMENTS}' = 'qa1' ]; then
+  #     sed -i 's/ip/${QA1_IP}/g' '${ANSIBLE_INVENTORY_FILE}'
+  #  elif [ '${params.ENVIRONMENTS}' = 'qa2' ]; then
+  #     sed -i 's/ip/${QA2_IP}/g' '${ANSIBLE_INVENTORY_FILE}'
+  #  elif [ '${params.ENVIRONMENTS}' = 'sync' ]; then
+  #     cp -r '${INVENTOR_DIR}'/sync/'${ANSIBLE_INVENTORY_FILE}' .
+  #     sed -i 's/ip1/${QA1_IP}/g' '${ANSIBLE_INVENTORY_FILE}'
+  #     sed -i 's/ip2/${QA2_IP}/g' '${ANSIBLE_INVENTORY_FILE}'
+  #  else
+  #  echo 'You have not selected any relevant environment'
+  #  fi
+
 """
     }
 }
