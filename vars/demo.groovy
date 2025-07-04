4b9ad271a40a9f383c5fc0a822a24791e2dd84ab
munawarraza <munawarraza068@gmail.com>
2025-07-05 01:54:55 +0500

    add

--- vars/demo.groovy
+++ vars/demo.groovy
@@ -4,3 +4,6 @@ def function1(){
 def function2(String name, String email){
   sh "echo Hi ${name}!, Your email is ${email}"
 }
+def function3(Map config){
+  sh "echo This is your IP in demo file ${config.SERVICE_IPS.DEV_IP}"
+}
