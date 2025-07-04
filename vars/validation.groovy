d78562b2aba39d51894cdbe745c699664725caef
munawarraza <munawarraza068@gmail.com>
2025-07-05 01:00:58 +0500

    add

--- vars/validation.groovy
+++ vars/validation.groovy
@@ -1,5 +1,6 @@
-def call(Map config){
+def call(Map params, Map config){
 	echo "Registry: ${config.DOCKER_REGISTRY}"
+	echo "Your Job name is ${env.JOB_NAME}"
   sh """
     echo "Deploying image version ${config.IMAGE_VERSION} to ${config.DOCKER_REGISTRY}"
   """
