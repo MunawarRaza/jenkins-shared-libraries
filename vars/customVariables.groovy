1db67364af95662d72c5fc5747d386f348bb8a9b
munawarraza <munawarraza068@gmail.com>
2025-07-05 01:48:14 +0500

    add

--- vars/customVariables.groovy
+++ vars/customVariables.groovy
@@ -15,10 +15,14 @@ def call() {
     DEVOPS_USERS: ['munawar', 'waqas'],
     DEVOPS_DEV_USERS: ['munawar', 'waqas', 'rehan', 'ali', 'sulman', 'maria', 'developer'],
     ENVIRONMENTS: ['dev', 'qa1', 'qa2', 'uat', 'uat1', 'uat2', 'sync'],
-//    ENVIRONMENTS: ['qa', 'staging', 'prod'],
     DOCKER_REGISTRY: 'docker.oneloadpk.com',
     IMAGE_VERSION: 'v1.0.0',
     DEPLOY_TIMEOUT: 300,
+    SERVICE_IPS: [
+	DEV_IP: "10.130.21.22",
+	QA1_IP: "10.130.22.22",
+	QA2_IP: "10.130.23.22"
+	],
     // add more variables as needed
   ]
 }
