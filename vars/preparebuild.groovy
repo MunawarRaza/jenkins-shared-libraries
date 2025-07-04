44919b5748f03e642e424b49fb33b4ccd82535df
munawarraza <munawarraza068@gmail.com>
2025-07-05 02:26:04 +0500

    add

--- vars/preparebuild.groovy
+++ vars/preparebuild.groovy
@@ -1,6 +1,7 @@
 def call(Map config){
     script {
 sh"""
+    rm -f tags.txt
     echo tag_name: '${params.TAG}' >>tags.txt
     echo job_name: '${JOB_NAME}' >>tags.txt
     echo env_name: '${params.ENVIRONMENTS}' >>tags.txt
